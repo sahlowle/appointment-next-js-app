c26a6a6d6aac49ef82aa1d4469d803de64079d25
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2023-01-23 12:53:54 +0200

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,9 +3,9 @@ import Axios from 'axios'
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
-        'X-Requested-With': 'XMLHttpRequest',
+        // 'X-Requested-With': 'XMLHttpRequest',
     },
-    withCredentials: true,
+    // withCredentials: true,
 })
 
 export default axios
